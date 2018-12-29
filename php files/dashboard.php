5a33ae5eca468d2c5907b142d76f6edebb2c85b1
SongoMen <songomengaming@gmail.com>
2018-12-29 06:41:33 +0100

    fixed view for firefox

--- php files/dashboard.php
+++ php files/dashboard.php
@@ -15,7 +15,7 @@ $name = $user_check;
 if(isset($_POST['submit'])){
     $taskName=$_POST['task-name'];
     $taskDate=$_POST['task-date'];
-    
+
     $query = "INSERT INTO $name (`Task`,`Task_date`) VALUES ('$taskName', '$taskDate')";
     header('Location: dashboard.php');
     mysqli_query($conn, $query);
@@ -40,35 +40,34 @@ mysqli_close($conn);
 </head>
 <body>
     <div id = "black-bg" class="black-bg" hidden>
-    </div>   
+    </div>
     <div class="context" hidden>
-        <div class="context_item"> 
+        <div class="context_item">
             <div class="inner_item" id="newTask">
             Create new task
-            </div> 
+            </div>
         </div>
-        <div class="context_item"> 
+        <div class="context_item">
             <div class="inner_item">
             Cut
-            </div> 
+            </div>
         </div>
-        <div class="context_item"> 
+        <div class="context_item">
             <div class="inner_item">
             Paste
-            </div> 
+            </div>
         </div>
         <div class="context_hr"></div>
-        <div class="context_item"> 
+        <div class="context_item">
             <div class="inner_item last" id="logout">
             Log Out
-            </div> 
+            </div>
         </div>
     </div>
     <div class="left-bar">
     <h1 class="bar__welcome">Welcome <p><?php echo $user_check; ?></p> </h1>
         <ul>
             <li style='color:#f44242;'><svg width="30" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" x="0px" y="0px" viewBox="0 0 100 125"><g transform="translate(0,-952.36218)"><path style="fill:#f44242; text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;enable-background:accumulate;" d="m 49.64027,963.37875 a 3.0003,3.0003 0 0 0 -1,0.3125 L 5.6402699,985.69124 a 3.0003,3.0003 0 0 0 0,5.3438 L 48.64027,1013.035 a 3.0003,3.0003 0 0 0 2.75,0 l 42.999996,-21.99996 a 3.0003,3.0003 0 0 0 0,-5.3438 L 51.39027,963.69125 a 3.0003,3.0003 0 0 0 -1.75,-0.3125 z m 0.375,6.37499 36.406196,18.625 -36.406196,18.59376 -36.4062,-18.59376 36.4062,-18.625 z m -43.0625001,29.5938 a 3.0003,3.0003 0 0 0 -1.3125,5.68746 l 43.0000001,22 a 3.0003,3.0003 0 0 0 2.75,0 l 42.999996,-22 a 3.0049417,3.0049417 0 1 0 -2.75,-5.34376 L 50.01527,1020.9725 8.3902699,999.69124 a 3.0003,3.0003 0 0 0 -1.4375,-0.3437 z m 0,13.99996 a 3.0003,3.0003 0 0 0 -1.3125,5.6875 l 43.0000001,22 a 3.0003,3.0003 0 0 0 2.75,0 l 42.999996,-22 a 3.0049417,3.0049417 0 1 0 -2.75,-5.3438 L 50.01527,1034.9725 8.3902699,1013.6912 a 3.0003,3.0003 0 0 0 -1.4375,-0.3437 z" fill="#000000" fill-opacity="1" stroke="none" marker="none" visibility="visible" display="inline" overflow="visible"/></g></svg>Dashboard</li>
-            <li><svg width="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 512 640" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><g><path d="M256,331.6c-41.7,0-75.6-33.9-75.6-75.6s33.9-75.6,75.6-75.6s75.6,33.9,75.6,75.6S297.7,331.6,256,331.6z      M256,219.1c-20.3,0-36.9,16.5-36.9,36.9s16.5,36.9,36.9,36.9s36.9-16.5,36.9-36.9S276.3,219.1,256,219.1z"/></g><path d="M335.9,457H176.1c-22.2,0-42.9-12-54-31.2v0l-80-138.6c-11.1-19.2-11.1-43.1,0-62.3l80-138.6    C133.2,67,153.9,55,176.1,55h159.9c22.2,0,42.9,12,54,31.2l80,138.6c11.1,19.2,11.1,43.1,0,62.3l-80,138.6    C378.8,445,358.1,457,335.9,457z M155.6,406.4c4.2,7.3,12,11.8,20.5,11.8h159.9c8.4,0,16.2-4.5,20.5-11.8l80-138.6    c4.2-7.3,4.2-16.3,0-23.6l-80-138.6c-4.2-7.3-12-11.8-20.5-11.8H176.1c-8.4,0-16.2,4.5-20.5,11.8l-80,138.6    c-4.2,7.3-4.2,16.3,0,23.6L155.6,406.4L155.6,406.4z"/></g></g></svg>Settings</li>
             <li><svg style="margin-bottom:5px;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 482.9 482.9" xml:space="preserve" width="30px" height="30px">
                     <g>
                         <path d="M239.7,260.2c0.5,0,1,0,1.6,0c0.2,0,0.4,0,0.6,0c0.3,0,0.7,0,1,0c29.3-0.5,53-10.8,70.5-30.5    c38.5-43.4,32.1-117.8,31.4-124.9c-2.5-53.3-27.7-78.8-48.5-90.7C280.8,5.2,262.7,0.4,242.5,0h-0.7c-0.1,0-0.3,0-0.4,0h-0.6    c-11.1,0-32.9,1.8-53.8,13.7c-21,11.9-46.6,37.4-49.1,91.1c-0.7,7.1-7.1,81.5,31.4,124.9C186.7,249.4,210.4,259.7,239.7,260.2z     M164.6,107.3c0-0.3,0.1-0.6,0.1-0.8c3.3-71.7,54.2-79.4,76-79.4h0.4c0.2,0,0.5,0,0.8,0c27,0.6,72.9,11.6,76,79.4    c0,0.3,0,0.6,0.1,0.8c0.1,0.7,7.1,68.7-24.7,104.5c-12.6,14.2-29.4,21.2-51.5,21.4c-0.2,0-0.3,0-0.5,0l0,0c-0.2,0-0.3,0-0.5,0    c-22-0.2-38.9-7.2-51.4-21.4C157.7,176.2,164.5,107.9,164.6,107.3z" />
@@ -101,7 +100,7 @@ mysqli_close($conn);
                             $color = "white";
                         }
                         $idName = $row["Task"];
-                        echo "<div style = 'color: $color;transition: all .5s' class='todo__elem $idName'><div style='display:flex; width:100%; align-items:center; justify-content: 
+                        echo "<div style = 'color: $color;transition: all .5s' class='todo__elem $idName'><div style='display:flex; width:100%; align-items:center; justify-content:
                         space-between;'>" . $row["Task"] . "
                         <input type='checkbox' class='table__items' id='$idName' name='$idName' $input value='' />
                         <label style='padding:10px;' for='$idName'>
